4b9e861474210bd58c73829f3f8abd4da6bf5ff4
mineachem <minea.chem@gmail.com>
2016-09-01 11:55:57 +0700

    add new website

---
+++ seeyourslide/src/main/java/com/seeyourslide/SeeyourslideApplication.java
@@ -0,0 +1,12 @@
+package com.seeyourslide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeeyourslideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeeyourslideApplication.class, args);
+	}
+}
